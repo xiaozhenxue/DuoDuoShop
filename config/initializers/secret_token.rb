34c9db6e8a06f70b16349188a238ac48a75058de
xiaozhen xue <xxuettu@gmail.com>
2016-11-10 11:59:59 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DuoDuoShop::Application.config.secret_token = 'db42821e4f832a9ab762e99007f833c4cffe74a2f4678abe43d5dc96d46dd20e2e3ccc98a36d7c21f2d1dfc887513f87c98db8402aef7f4f98c5aa2f8397c674'
